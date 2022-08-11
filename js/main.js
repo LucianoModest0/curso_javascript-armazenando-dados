d8d24194cedc243426f1f80f47c5d21ff7842c4d
Luciano Modesto <modesto-l@live.com>
2022-08-11 11:22:34 -0300

    Att: Removing items from browser and localStorage

--- js/main.js
+++ js/main.js
@@ -24,9 +24,11 @@ form.addEventListener("submit", (event) => {
 
     updateElement(currentItem);
 
-    items[exists.id] = currentItem;
+    items[items.findIndex((element) => element.id === exists.id)] = currentItem;
   } else {
-    currentItem.id = items.length;
+    currentItem.id = items[items.length - 1]
+      ? items[items.length - 1].id + 1
+      : 0;
 
     createElement(currentItem);
 
@@ -52,10 +54,34 @@ function createElement(item) {
 
   newItem.innerHTML += item.nome;
 
+  newItem.appendChild(deleteButton(item.id));
+
   list.appendChild(newItem);
 }
 
 function updateElement(item) {
   document.querySelector("[data-id='" + item.id + "']").innerHTML =
     item.quantidade;
+}
+
+function deleteButton(id) {
+  const buttonElement = document.createElement("button");
+  buttonElement.innerText = "x";
+
+  buttonElement.addEventListener("click", function () {
+    deleteElement(this.parentNode, id);
+  });
+
+  return buttonElement;
+}
+
+function deleteElement(tag, id) {
+  tag.remove();
+
+  items.splice(
+    items.findIndex((element) => element.id === id),
+    1
+  );
+
+  localStorage.setItem("itens", JSON.stringify(items));
 }
